9cf55c8a5d8b09e35b4939c02e1c5782bd605fca
Wooj Choi <zomdar@gmail.com>
2020-05-10 23:41:56 -0400

    updating app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.send('whats up dawg');
+	res.send('whats up dawg!');
 });
 
 app.listen(port, () => console.log(`zomdar app listening on port ${port}!`));
